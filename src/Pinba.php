b2622edc00a93d0611b6684fe0b58fe5d6ef8c27
Венделев Артем <avendelev@corp.local>
2018-02-09 13:02:06 +0300

    Move timers to a separate class

--- src/Pinba.php
+++ src/Pinba.php
@@ -3,8 +3,6 @@
 namespace elementary\monitoring;
 
 use elementary\core\Singleton\SingletonTrait;
-use InvalidArgumentException;
-use OutOfRangeException;
 
 class Pinba
 {
@@ -16,75 +14,56 @@ class Pinba
     private $enabled;
 
     /**
-     * @var array
-     */
-    private $timers = array();
-
-
-    /**
-     * @param string $name Внутреннее имя, не передается на сервер Pinba. Требуется для остановки и удаления timer
      * @param array $tags
      *
-     * @return $this
-     * @throws InvalidArgumentException
+     * @return bool|resource
      */
-    public function timerStart($name, array $tags)
+    public function timerStart(array $tags)
     {
         if ($this->isEnabled()) {
-            if ($this->isTimerExists($name)) {
-                throw new InvalidArgumentException('Такой timer уже существет: '. $name);
-            }
-
-            $this->setTimer($name, pinba_timer_start($tags));
+            return pinba_timer_start($tags);
+        } else {
+            return true;
         }
-
-        return $this;
     }
 
     /**
-     * @param string $name
+     * @param resource $timer
      *
-     * @return $this
-     * @throws OutOfRangeException
+     * @return bool
      */
-    public function timerStop($name)
+    public function timerStop($timer)
     {
         if ($this->isEnabled()) {
-            pinba_timer_stop($this->getTimer($name));
-
-            $this->unsetTimer($name);
+            return pinba_timer_stop($timer);
+        } else {
+            return true;
         }
-
-        return $this;
     }
 
     /**
-     * @param string $name
+     * @param resource $timer
      *
-     * @return $this
-     * @throws OutOfRangeException
+     * @return bool
      */
-    public function timerDelete($name)
+    public function timerDelete($timer)
     {
         if ($this->isEnabled()) {
-            pinba_timer_delete($this->getTimer($name));
+            return pinba_timer_delete($timer);
+        } else {
+            return true;
         }
-
-        $this->unsetTimer($name);
-
-        return $this;
     }
 
     /**
-     * @param string $name
+     * @param resource $timer
      *
      * @return array
-     * @throws OutOfRangeException
      */
-    public function timerGetInfo($name)
+    public function timerGetInfo($timer)
     {
         if ($this->isEnabled()) {
-            return pinba_timer_get_info($this->getTimer($name));
+            return pinba_timer_get_info($timer);
         } else {
             return [];
         }
@@ -145,126 +124,6 @@ class Pinba
 
         return $this;
     }
-
-    /**
-     * @return array
-     */
-    protected function getTimers()
-    {
-        return $this->timers;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return resource
-     * @throws OutOfRangeException
-     */
-    protected function getTimer($name)
-    {
-        if ($this->isTimerExists($name)) {
-            throw new OutOfRangeException('Timer не найден: ' . $name);
-        }
-
-        return $this->timers[$name];
-    }
-
-    /**
-     * @param string   $name
-     * @param resource $value
-     *
-     * @return $this
-     */
-    protected function setTimer($name, $value)
-    {
-        $this->timers[$name] = $value;
-
-        return $this;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return $this
-     */
-    protected function unsetTimer($name)
-    {
-        unset($this->timers[$name]);
-
-        return $this;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return bool
-     */
-    public function isTimerExists($name)
-    {
-        return array_key_exists($name, $this->getTimers());
-    }
-
-//    public function timerStart($name, array $tags, array $data = array())
-//    {
-//        if (array_key_exists($name, $this->timers))
-//            throw new WrongArgumentException('a timer with the same name allready exists');
-//
-//        if ($this->isTreeLogEnabled()) {
-//
-//            $id = uniqid();
-//            $tags['treeId'] = $id;
-//
-//            if (!empty($this->queue))
-//                $tags['treeParentId'] = end($this->queue);
-//            else
-//                $tags['treeParentId'] = 'root';
-//
-//            $this->queue[] = $id;
-//        }
-//
-//        $this->timers[$name] =
-//            count($data)
-//                ? pinba_timer_start($tags, $data)
-//                : pinba_timer_start($tags);
-//
-//        return $this;
-//    }
-
-//    public function timerStop($name)
-//    {
-//        if ($this->isTreeLogEnabled())
-//            array_pop($this->queue);
-//
-//        if (!array_key_exists($name, $this->timers))
-//            throw new WrongArgumentException('have no any timer with name '.$name);
-//
-//        pinba_timer_stop($this->timers[$name]);
-//
-//        unset($this->timers[$name]);
-//
-//        return $this;
-//    }
-
-
-//    public function timerDelete($name)
-//    {
-//        if (!array_key_exists($name, $this->timers))
-//            throw new WrongArgumentException('have no any timer with name '.$name);
-//
-//        pinba_timer_delete($this->timers[$name]);
-//
-//        unset($this->timers[$name]);
-//
-//        return $this;
-//    }
-//
-//    public function timerGetInfo($name)
-//    {
-//        if (!array_key_exists($name, $this->timers))
-//            throw new WrongArgumentException('have no any timer with name '.$name);
-//
-//        return pinba_timer_get_info($this->timers[$name]);
-//    }
 
     /**
      * @return bool
