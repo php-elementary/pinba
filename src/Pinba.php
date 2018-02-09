3066e2251e9dafc8aaef1c6e102ab36511355b8e
Artiom <vend_arty@mail.ru>
2018-02-09 13:06:06 +0300

    Pinba and Timers (#1)

---
+++ src/Pinba.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace elementary\monitoring;
+
+use elementary\core\Singleton\SingletonTrait;
+
+class Pinba
+{
+    use SingletonTrait;
+
+    /**
+     * @var bool
+     */
+    private $enabled;
+
+    /**
+     * @param array $tags
+     *
+     * @return bool|resource
+     */
+    public function timerStart(array $tags)
+    {
+        if ($this->isEnabled()) {
+            return pinba_timer_start($tags);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @param resource $timer
+     *
+     * @return bool
+     */
+    public function timerStop($timer)
+    {
+        if ($this->isEnabled()) {
+            return pinba_timer_stop($timer);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @param resource $timer
+     *
+     * @return bool
+     */
+    public function timerDelete($timer)
+    {
+        if ($this->isEnabled()) {
+            return pinba_timer_delete($timer);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @param resource $timer
+     *
+     * @return array
+     */
+    public function timerGetInfo($timer)
+    {
+        if ($this->isEnabled()) {
+            return pinba_timer_get_info($timer);
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setScriptName($name)
+    {
+        if ($this->isEnabled()) {
+            pinba_script_name_set($name);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setServerName($name)
+    {
+        if ($this->isEnabled()) {
+            pinba_server_name_set($name);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setHostName($name)
+    {
+        if ($this->isEnabled()) {
+            pinba_hostname_set($name);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Ручная запись данных
+     *
+     * @return $this
+     */
+    public function flush()
+    {
+        if ($this->isEnabled()) {
+            pinba_flush();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        if ($this->getEnabled() === null) {
+            $this->setEnabled(ini_get("pinba.enabled") === "1");
+        }
+
+        return $this->getEnabled();
+    }
+
+    /**
+     * @return bool
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     *
+     * @return $this
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+}
---
+++ src/Timers.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace elementary\monitoring;
+
+use elementary\core\Singleton\SingletonTrait;
+use InvalidArgumentException;
+use OutOfRangeException;
+
+class Timers
+{
+    use SingletonTrait;
+
+    /** @var array */
+    protected $timers = [];
+
+    /**
+     * @param string $key The internal key is not sent to the Pinba server.
+     * @param array  $tags Array of tags and their values in the form of "tag" => "value". Cannot contain numeric indexes for obvious reasons.
+     *
+     * @return $this
+     */
+    public function start($key, array $tags=[])
+    {
+        if ($this->isExists($key)) {
+            throw new InvalidArgumentException('Timer is allready exists: '. $key);
+        }
+
+        $this->setTimer($key, $this->pinba()->timerStart($tags));
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return $this
+     */
+    public function stop($key)
+    {
+        $this->pinba()->timerStop($this->getTimer($key));
+        $this->unsetTimer($key);
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return $this
+     */
+    public function delete($key)
+    {
+        $this->pinba()->timerDelete($this->getTimer($key));
+        $this->unsetTimer($key);
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return array
+     */
+    public function info($key)
+    {
+        return $this->pinba()->timerGetInfo($this->getTimer($key));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTimers()
+    {
+        return $this->timers;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return resource
+     * @throws OutOfRangeException
+     */
+    protected function getTimer($key)
+    {
+        if ($this->isExists($key)) {
+            throw new OutOfRangeException('Timer not found: ' . $key);
+        }
+
+        return $this->timers[$key];
+    }
+
+    /**
+     * @param string   $key
+     * @param resource $value
+     *
+     * @return $this
+     */
+    protected function setTimer($key, $value)
+    {
+        $this->timers[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return $this
+     */
+    protected function unsetTimer($key)
+    {
+        unset($this->timers[$key]);
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function isExists($key)
+    {
+        return array_key_exists($key, $this->getTimers());
+    }
+
+    public function pinba()
+    {
+        return Pinba::me();
+    }
+}
